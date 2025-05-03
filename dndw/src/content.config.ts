48850c9565f896120117b8757bc04766313be691
Nathan Lee <me@nwlee.tech>
2025-05-02 19:46:36 -0500

    continue update

--- dndw/src/content.config.ts
+++ dndw/src/content.config.ts
@@ -7,11 +7,7 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		description: z.string()
 	}),
 });
 
