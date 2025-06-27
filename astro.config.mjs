27bd87aaf117644b70cd3e887dabf7d60e6abe4a
Nathan Lee <me@nwlee.tech>
2025-06-27 10:42:54 -0500

    new blog postgit add .!

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dndw.nwlee.tech',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		rehypePlugins: [
-			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+			rehypeKatex
+		],
+		remarkPlugins: [
+			remarkMath
 		]
 	}
 });
